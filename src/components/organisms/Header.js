9e300348f5ed016bb9da062bc2bbd7dd6fcfbf08
rickrd <desouzaricardoc@gmail.com>
2020-04-10 15:51:15 -0300

    Added Logout & Welcome to header

--- src/components/organisms/Header.js
+++ src/components/organisms/Header.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import Logout from '../atoms/Logout'
+import Welcome from '../atoms/Welcome'
+
 const HeaderWrapper = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: space-between;
   padding: 16px;
   background-color: #212121;
   color: #fff;
+
+  @media (min-width: 1024px) {
+    flex-direction: row;
+  }
 `
 
 const Header = () => {
   return (
     <HeaderWrapper>
       <div>CONDOMINIUM MANAGEMENT</div>
-      <div>LOGOUT</div>
+      <Welcome></Welcome>
+      <Logout></Logout>
     </HeaderWrapper>
   )
 }
