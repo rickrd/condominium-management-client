bb4e3c84aba2008800170ca9f0a35cdebeff6228
rickrd <desouzaricardoc@gmail.com>
2020-04-09 22:36:12 -0300

    Added user login status action

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,10 @@
-export const UPDATE_USER_EMAIL = 'UPDATE_USER'
+export const UPDATE_USER_EMAIL = 'UPDATE_USER_EMAIL'
+export const UPDATE_USER_LOGIN_STATUS = 'UPDATE_USER_LOGIN_STATUS'
 
 export const updateUserEmail = (email) => {
   return { type: UPDATE_USER_EMAIL, email }
+}
+
+export const updateUserLoginStatus = (status) => {
+  return {type: UPDATE_USER_LOGIN_STATUS, status}
 }
