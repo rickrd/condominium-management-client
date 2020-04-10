9801b1e847d77359eb2a610bba23f6b5214f8873
rickrd <desouzaricardoc@gmail.com>
2020-04-10 20:34:45 -0300

    Modified home to render apartments & residents

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import Header from '../organisms/Header'
+import ApartmentsList from '../organisms/ApartmentsList'
 
 const HomeWrapper = styled.div`
   margin: 0;
   font-family: 'Roboto';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background-color: #FAFAFF;
+  color: #212121;
 `
 
 const Home = () => {
   return (
     <HomeWrapper>
-      <Header></Header>
-      <div>This is the home</div>
+      <h2>LIST OF ALL APARTMENTS:</h2>
+      <ApartmentsList></ApartmentsList>
+      <h2>LIST OF ALL RESIDENTS:</h2>
+      <div>Residents List</div>
     </HomeWrapper>
   )
 }
