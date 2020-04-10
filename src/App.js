24481565c311591499c806f5cd8b9b6feedc70d4
rickrd <desouzaricardoc@gmail.com>
2020-04-09 22:32:50 -0300

    Verified user authentication before rendering pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 
 import Home from './components/pages/Home'
+import LoginForm from './components/organisms/LoginForm'
+import {getAuthentication} from './contexts/Authentication'
+import {updateUserEmail} from './redux/actions'
+import {updateUserLoginStatus} from './redux/actions'
 
-import ContextProvider from './contexts/Authentication'
 
-function App() {
-  return <ContextProvider></ContextProvider>
+function App(props) {
+  console.log(props)
+  const { user, dispatch } = props
+  console.log(user)
+
+  if (!user.loginStatus) {
+    getAuthentication()
+      .then((res) => {
+        console.log(res)
+        dispatch(updateUserLoginStatus({res}))
+      })
+    return <LoginForm></LoginForm>
+  }
+
+  return (
+      <Home></Home>
+  )
 }
 
-export default App
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(App)
