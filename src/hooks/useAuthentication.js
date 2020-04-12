f8f450f4e105c7894b5a9e7ffe2b5f4549188aa7
rickrd <desouzaricardoc@gmail.com>
2020-04-11 23:23:46 -0300

    Remove Authentication file, Create useAuthentication hook

---
+++ src/hooks/useAuthentication.js
@@ -0,0 +1,41 @@
+import { Magic } from 'magic-sdk'
+
+const useAuthentication = () => {
+  const magic = new Magic(`${process.env.REACT_APP_MAGIC_KEY}`)
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    const email = new FormData(e.target).get('email')
+    if (email) {
+      const token = await magic.auth.loginWithMagicLink({ email })
+
+      return token
+    }
+  }
+
+  const handleLogout = async () => {
+    const isLoggedOut = await magic.user.logout()
+
+    return isLoggedOut
+  }
+
+  const getAuthentication = async () => {
+    const { user } = magic
+
+    const isAuthenticated = await user.isLoggedIn()
+
+    return isAuthenticated
+  }
+
+  const getUserMetadata = async () => {
+    const { user } = magic
+
+    const magicUser = await user.getMetadata()
+
+    return magicUser
+  }
+
+  return { handleLogin, handleLogout, getAuthentication, getUserMetadata }
+}
+
+export { useAuthentication }
