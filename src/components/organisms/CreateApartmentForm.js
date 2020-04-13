cb5ebc0da7cca4602212ff583fa4caee1028b4ca
rickrd <desouzaricardoc@gmail.com>
2020-04-12 22:55:40 -0300

    Add formik to create aparment form and init graphql mutation

--- src/components/organisms/CreateApartmentForm.js
+++ src/components/organisms/CreateApartmentForm.js
@@ -1,39 +1,102 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { Formik, Form, FieldArray } from 'formik'
+import gql from 'graphql-tag'
+import { useMutation } from '@apollo/react-hooks'
+// $number: Int!, $block: String!, $residents: [ResidentInput]!
+const CREATE_APARTMENT = gql`
+  mutation createApartment($input: CreateApartmentInput!) {
+    createApartment(input: $input) {
+      apartment {
+        number
+        block
+        residents {
+          name
+        }
+      }
+    }
+  }
+`
 
 const CreateApartmentForm = () => {
-  const [inputs, setInputs] = useState(['input-0'])
-
-  function handleDeleteInput(e) {
-
-  }
-
-  function handleAddInput(e) {
-    e.preventDefault()
-    setInputs([...inputs, `input-${inputs.length}`])
-  }
-
-  function handleFormSubmit(e){
-    e.preventDefault()
-    console.log(e.target)
+  const [createApartment, { data }] = useMutation(CREATE_APARTMENT)
+  const initialValues = {
+    number: '',
+    block: '',
+    residents: [
+      { name: 'Ricardo', birthdate: '28/04/1999', phone: '992577885', email: 'desouzaricardoc@gmail.com' },
+      { name: 'Ricardo', birthdate: '28/04/1999', phone: '992577885', email: 'desouzaricardoc@gmail.com' },
+    ],
   }
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      <h1>Create apartment:</h1>
-      <input type="text" name="number" placeholder="Number"></input>
-      <input type="text" name="block" placeholder="Block"></input>
-      {inputs.map((input) => (
-        <div key={input}>
-          <input type="text" name="name" placeholder="Resident name"></input>
-          <input type="text" name="birthdate" placeholder="Resident birthdate"></input>
-          <input type="text" name="phone" placeholder="Resident phone"></input>
-          <input type="text" name="email" placeholder="Resident e-mail"></input>
-          <span onClick={handleDeleteInput}>X</span>
-        </div>
-      ))}
-      <button onClick={handleAddInput}>Add Resident</button>
-      <button type="submit">Send</button>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      validate={(values) => {
+        const errors = {}
+        Object.keys(initialValues).map((propertyName) => {
+          if (!values[propertyName]) {
+            errors[propertyName] = `The field ${propertyName} is required.`
+          }
+        })
+        if (values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+          errors.email = 'Invalid email address'
+        }
+        return errors
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          createApartment({
+            variables: {
+              input: values,
+            },
+          })
+          setSubmitting(false)
+        }, 400)
+      }}
+    >
+      {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+        <Form>
+          <h1>Create apartment:</h1>
+          <input type="text" name="number" placeholder="Number" onChange={handleChange} onBlur={handleBlur} value={values.number} />
+          {errors.number && touched.number && errors.number}
+          <input type="text" name="block" placeholder="Block" onChange={handleChange} onBlur={handleBlur} value={values.block} />
+          {errors.block && touched.block && errors.block}
+          <FieldArray
+            name="residents"
+            render={(arrayHelpers) => (
+              <div>
+                {values.residents.map((resident, index) => (
+                  <div key={index}>
+                    <input name={`residents.${index}.name`} onChange={handleChange} onBlur={handleBlur} value={values.residents[index].name} />
+                    <input name={`residents.${index}.birthdate`} onChange={handleChange} onBlur={handleBlur} value={values.residents[index].birthdate} />
+                    <input name={`residents.${index}.phone`} onChange={handleChange} onBlur={handleBlur} value={values.residents[index].phone} />
+                    <input name={`residents.${index}.email`} onChange={handleChange} onBlur={handleBlur} value={values.residents[index].email} />
+                    <button
+                      type="button"
+                      onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                    >
+                      -
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
+                    >
+                      +
+                    </button>
+                  </div>
+                ))}
+                <button type="button" onClick={() => arrayHelpers.push('')}>
+                  Add a resident
+                </button>
+              </div>
+            )}
+          ></FieldArray>
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
   )
 }
 
